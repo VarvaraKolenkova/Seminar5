db674e2c56ffd66e5cd173a0b1e8a8cc8a1af63a
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-30 08:38:43 +0300

    Разница между максимальным и минимальным элементом в вещественном массиве

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Разница между максимальным и минимальным элементом массива вещественных чисел
+double[] array = {5.1, 8.2, 9.9, 25.8, 3.14, 14.2};
+
+double FindMax(double[] array)
+{
+double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+    }
+return max;
+}
+
+double FindMin(double[] array)
+{
+double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < min) min = array[i];
+    }
+return min;
+}
+System.Console.WriteLine($"Max element: {FindMax(array)}");
+System.Console.WriteLine($"Min element: {FindMin(array)}");
+System.Console.WriteLine($"Difference Max-Min: {FindMax(array) - FindMin(array)}");
